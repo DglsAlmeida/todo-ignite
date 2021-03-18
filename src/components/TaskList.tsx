eda30d1caa076a9ae1ce98119fe5b4d4323681d6
rivand <rivand@RIVAND1>
2021-03-17 22:27:55 -0300

    challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,10 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
+    if(!newTaskTitle) {
+      return null;
+    }
+    
     setTasks([...tasks, {
       id: uuidv4(),
       title: newTaskTitle,
